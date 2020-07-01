ea5a359eded3856e509b25e83d9495015ae00c3b
Nicolas Cervantes <50224828+nicolasc06@users.noreply.github.com>
2020-06-30 21:28:06 -0500

    Creacion mockup conexión a robot

---
+++ Android/app/src/main/java/com/example/cobot/RobotConection.java
@@ -0,0 +1,14 @@
+package com.example.cobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RobotConection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robot_conection);
+    }
+}
