fcd9a1ca9ca181812c84273cf7e3cecc79cbee7f
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-07-02 12:58:51 -0500

    toast

--- Android/app/src/main/java/com/example/cobot/RobotConection.java
+++ Android/app/src/main/java/com/example/cobot/RobotConection.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RobotConection extends AppCompatActivity {
     Button continuar;
@@ -23,6 +24,7 @@ public class RobotConection extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
+                Toast.makeText(v.getContext(), "Robot conectado", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
         });
