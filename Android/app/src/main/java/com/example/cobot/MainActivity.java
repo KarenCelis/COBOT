4f25218743ebf4a29d47fc0b2bb44e98af971620
pedro-escobar <pedromartinescobargomez@gmail.com>
2020-07-02 08:59:29 -0500

    Elaborada la pantalla del Mockup Cero, pendientes las interacciones con los objetos.

--- Android/app/src/main/java/com/example/cobot/MainActivity.java
+++ Android/app/src/main/java/com/example/cobot/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.cobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Continuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Continuar = findViewById(R.id.BotonContinuarMainActivity);
+        Continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent TrancisionConfigureAction = new Intent(Continuar.getContext(), RobotConection.class);
+                startActivity(TrancisionConfigureAction);
+            }
+        });
     }
 }
