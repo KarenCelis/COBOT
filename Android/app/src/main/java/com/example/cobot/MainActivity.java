f1e03367e2814151f62c3b4dfe4d6963d0ec8ebd
pedro-escobar <pedromartinescobargomez@gmail.com>
2020-08-05 21:43:42 -0500

    Eliminadas las pantallas de los prototipos obsoletos.

--- Android/app/src/main/java/com/example/cobot/MainActivity.java
+++ Android/app/src/main/java/com/example/cobot/MainActivity.java
@@ -11,48 +11,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ContinuarM;
-    Button buscarArchivo;
-    TextView textoDeArchivoDeObra;
-    Intent myFileIntent;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ContinuarM = findViewById(R.id.BotonContinuarMainActivity);
-        buscarArchivo = findViewById(R.id.BuscarArchivoDeObra);
-        textoDeArchivoDeObra = findViewById(R.id.TextoDeArchivoDeObra);
-        buscarArchivo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                myFileIntent.setType("*/*");
-                startActivityForResult(myFileIntent, 10);
-            }
-        });
-        ContinuarM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent TrancisionConfigureAction = new Intent(ContinuarM.getContext(), ConfigureAction.class);
-                startActivity(TrancisionConfigureAction);
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-
-            case 10:
-                if (resultCode== RESULT_OK) {
-                    String path = data.getData().getPath();
-                    textoDeArchivoDeObra.setText(path);
-
-                }
-                break;
-
-        }
     }
 }
