640b2563ea9f26f5f038177fc77ff561de3764a5
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-08-06 21:55:43 -0500

    botones de prueba en el main

--- Android/app/src/main/java/com/example/cobot/MainActivity.java
+++ Android/app/src/main/java/com/example/cobot/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.cobot;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-    }
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button ;
+    private Button save;
+    private Button boton;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicial = findViewById(R.id.btn_inicial);
+
+        inicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createContactDiaglog();
+            }
+        });
+
+
     }
-}
+
+    public void createContactDiaglog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View contactPopup = getLayoutInflater().inflate(R.layout.popup, null);
+        boton = contactPopup.findViewById(R.id.btn_opc);
+        save = contactPopup.findViewById(R.id.btn_save);
+        dialogBuilder.setView(contactPopup);
+        dialog = dialogBuilder.create();
+        dialog.show();
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+        });
+
+    }
