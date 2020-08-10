5ebd3b8b85d5bda7ee51bb3603120219036cfa74
pedro-escobar <pedromartinescobargomez@gmail.com>
2020-08-09 23:21:00 -0500

    Removidos los diálogos y puestos en la actividad Central.

--- Android/app/src/main/java/com/example/cobot/MainActivity.java
+++ Android/app/src/main/java/com/example/cobot/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
     private AlertDialog dialog;
     private static final int READ_REQUEST_CODE = 42;
     private static final String TAG = "FileRead";
-    private Button btnSonido,btnMirar,btnGirar,BCargarObra;
+    private Button BCargarObra;
     private Obra obra;
 
     @Override
@@ -39,10 +39,6 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         Button BAbrirArchivo = findViewById(R.id.BAbrirArchivo);
-        btnSonido = findViewById(R.id.btn_sonido);
-        btnMirar = findViewById(R.id.btn_mirar);
-        btnGirar = findViewById(R.id.btn_girar);
-        BCargarObra = findViewById(R.id.BCargarObra);
 
         BAbrirArchivo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,24 +52,6 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, READ_REQUEST_CODE);
             }
         });
-        btnSonido.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createContactDiaglogSonido();
-            }
-        });
-        btnMirar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createContactDiaglogMirar();
-            }
-        });
-        btnGirar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createContactDiaglogGirar();
-            }
-        });
     }
 
     @Override
@@ -85,6 +63,7 @@ public class MainActivity extends Activity {
                 assert uri != null;
                 String path = uri.getPath();
                 TextView TVSeleccionArchivo = findViewById(R.id.TVSeleccionarArchivo);
+                BCargarObra = findViewById(R.id.BCargarObra);
 
                 TVSeleccionArchivo.setText(path);
                 BCargarObra.setVisibility(View.VISIBLE);
@@ -109,14 +88,14 @@ public class MainActivity extends Activity {
     private void IniciarActividadDeSeleccionDePersonaje(Uri uri) throws IOException, JSONException {
         cargarObra(uri);
         Intent intent = new Intent(this, CharacterSelectionActivity.class);
-        //pasar la información de la uri de la obra para ser cargada mientras se crea la actividad.
+        //pasar la información de la obra.
         intent.putExtra("obra", obra);
         startActivity(intent);
     }
 
     private void cargarObra(Uri uri) throws IOException, JSONException {
         StringBuilder sb = new StringBuilder();
-        Log.i(TAG, "Uri path: "+uri.getPath());
+        Log.i(TAG, "Uri path: " + uri.getPath());
         InputStream inputStream = getContentResolver().openInputStream(uri);
         assert inputStream != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -128,120 +107,5 @@ public class MainActivity extends Activity {
         reader.close();
         String Result = sb.toString();
         obra = Reader.crearObraDesdeJSON(Result);
-    }
-
-    public void createContactDiaglogSonido() {
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View contactPopup = getLayoutInflater().inflate(R.layout.layout_sonido, null);
-        Button cantar = contactPopup.findViewById(R.id.btn_cantar);
-        Button gritar = contactPopup.findViewById(R.id.btn_gritar);
-        Button silbar = contactPopup.findViewById(R.id.btn_silbar);
-        Button reir = contactPopup.findViewById(R.id.btn_reir);
-        dialogBuilder.setView(contactPopup);
-        dialog = dialogBuilder.create();
-        dialog.show();
-        cantar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "cantar", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        gritar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "gritar", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        silbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "silbar", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        reir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "reir", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-
-    }
-
-    public void createContactDiaglogMirar() {
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View contactPopup = getLayoutInflater().inflate(R.layout.layout_mirar, null);
-        Button mirarDerecha = contactPopup.findViewById(R.id.btn_mirarDerec);
-        Button mirarIzquierda = contactPopup.findViewById(R.id.btn_mirarIzq);
-        Button mirarFrente = contactPopup.findViewById(R.id.btn_mirarFrente);
-
-        dialogBuilder.setView(contactPopup);
-        dialog = dialogBuilder.create();
-        dialog.show();
-        mirarDerecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "mirarDerecha", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        mirarIzquierda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "mirarIzquierda", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        mirarFrente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "mirarFrente", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-
-    }
-
-    public void createContactDiaglogGirar() {
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View contactPopup = getLayoutInflater().inflate(R.layout.layout_girar, null);
-        Button girarDerecha = contactPopup.findViewById(R.id.btn_girarDerech);
-        Button girarIzquierda = contactPopup.findViewById(R.id.btn_girarIzq);
-        Button girarFrente = contactPopup.findViewById(R.id.btn_girarFrente);
-        Button girarDeEspaldas = contactPopup.findViewById(R.id.btn_girarDeEspaldas);
-        dialogBuilder.setView(contactPopup);
-        dialog = dialogBuilder.create();
-        dialog.show();
-        girarDerecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "girarDerecha", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        girarIzquierda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "girarIzquierda", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        girarFrente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "girarFrente", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
-        girarDeEspaldas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "girarDeEspaldas", Toast.LENGTH_LONG).show();
-                dialog.dismiss();
-            }
-        });
     }
 }
