998dfa2735b88b71cdadf13f3b29e87e8de8cf2c
pedro-escobar <pedromartinescobargomez@gmail.com>
2020-08-09 23:24:40 -0500

    Agregada una actividad de selección de personaje con Spinner. Pendiente cambiar a Custom ListView para mejorar la apariencia.

--- Android/app/src/main/java/com/example/cobot/CharacterSelectionActivity.java
+++ Android/app/src/main/java/com/example/cobot/CharacterSelectionActivity.java
@@ -2,18 +2,28 @@ package com.example.cobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
+import com.example.cobot.Classes.Character;
 import com.example.cobot.Classes.Obra;
 
-import java.util.Objects;
+import java.util.ArrayList;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
 
     private Obra obra;
-    private static final String TAG = "FileLoad";
+    private static final String TAG = "ItemSelection";
+    private Spinner SPlistadoDePersonajes;
+    private int itemSelected;
+    private Button BContinuar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +31,45 @@ public class CharacterSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_character_selection);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        obra = (Obra)getIntent().getSerializableExtra("obra");
-        TextView TVNombreObra = findViewById(R.id.TVNombreObra);
-        TVNombreObra.setText(obra.getTitle());
+        BContinuar = findViewById(R.id.BContinuar);
+        BContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IniciarActividadCentral();
+            }
+        });
 
+        obra = (Obra)getIntent().getSerializableExtra("obra");
+
+        ArrayList<String> personajes = new ArrayList<>();
+        for(Character cha: obra.getCharacters()){
+            personajes.add(cha.getName());
+        }
+
+        SPlistadoDePersonajes = findViewById(R.id.SPListadoDePersonajes);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, personajes);
+        SPlistadoDePersonajes.setAdapter(adapter);
+        SPlistadoDePersonajes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFromList =(String) (SPlistadoDePersonajes.getItemAtPosition(position));
+                itemSelected = position+1;
+                Log.i(TAG, "El ítem seleccionado tiene la id: "+position+" es del personaje "+selectedFromList);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+    private void IniciarActividadCentral() {
+        Intent intent = new Intent(this, CentralActivity.class);
+        //pasar la información de la obra.
+        intent.putExtra("obra", obra);
+        intent.putExtra("itemSelected", itemSelected);
+        startActivity(intent);
+    }
+
 }
